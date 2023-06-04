dd49c6c59d606426a5b62837cee99739c53348eb
brendiaz03 <brendiaaz03@gmail.com>
2023-06-04 17:05:31 -0300

    Primer archivo

---
+++ Pilas/src/pbII/Pila.java
@@ -0,0 +1,20 @@
+package pbII;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Pila {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> pilis = new LinkedList<>();
+		
+		pilis.add(1);
+		pilis.add(2);
+		pilis.add(3);
+		
+		
+
+	}
+
+}
